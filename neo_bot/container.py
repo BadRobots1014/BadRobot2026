34d9ada7a826ec544828b587f4596e6b49f64c42
ReidS28 <strawj3434@gmail.com>
2026-02-07 10:23:01 -0500

    format

--- neo_bot/container.py
+++ neo_bot/container.py
@@ -14,6 +14,7 @@ import wpimath.filter
 import commands2
 from neo_bot import drivetrain
 
+
 class NeoBotContainer:
     def __init__(self) -> None:
         self.controller = wpilib.PS4Controller(0)
