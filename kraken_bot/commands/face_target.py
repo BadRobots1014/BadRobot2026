40f9cbaf8682bda27e11dcd211359858ed5287a9
TigerQa <mickelbick@gmail.com>
2026-02-07 12:27:22 -0500

    Fix merge

--- kraken_bot/commands/face_target.py
+++ kraken_bot/commands/face_target.py
@@ -1,5 +1,5 @@
 from commands2 import Command
-from commands2.button import CommandXboxController
+from commands2.button import CommandGenericHID
 from pathplannerlib.controller import PIDController
 from pathplannerlib.pathfinders import Translation2d
 from phoenix6.controls.twinkle_off_animation import math
@@ -12,16 +12,18 @@ def FaceTarget(
     swerve_subsystem: CommandSwerveDrivetrain,
     target_point: Translation2d,
     field: FieldCentric,
-    joystick: CommandXboxController,
+    joystick: CommandGenericHID,
     max_speed: float,
     max_angular_rate: float,
+    left_y_axis: int,
+    left_x_axis: int,
 ) -> Command:
     rotate_pid = PIDController(1, 0, 0)
     rotate_pid.enableContinuousInput(0, 2 * math.pi)
     return swerve_subsystem.apply_request(
         lambda: (
-            field.with_velocity_x(-joystick.getLeftY() * max_speed)
-            .with_velocity_y(-joystick.getLeftX() * max_speed)
+            field.with_velocity_x(-joystick.getRawAxis(left_y_axis) * max_speed)
+            .with_velocity_y(-joystick.getRawAxis(left_x_axis) * max_speed)
             .with_rotational_rate(
                 rotate_pid.calculate(
                     swerve_subsystem.get_state().pose.rotation().radians(),
--- kraken_bot/container.py
+++ kraken_bot/container.py
@@ -5,7 +5,8 @@
 #
 
 import commands2
-from commands2.button import CommandXboxController, Trigger
+import wpilib
+from commands2.button import CommandGenericHID, Trigger
 from pathplannerlib.auto import AutoBuilder
 from pathplannerlib.path import Translation2d
 from phoenix6 import swerve
@@ -25,6 +26,18 @@ class KrakenRobotContainer:
     periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
     subsystems, commands, and button mappings) should be declared here.
     """
+
+    # Controller axis mappings
+    LEFT_X_AXIS = 0
+    LEFT_Y_AXIS = 1
+    RIGHT_X_AXIS = 2 if wpilib.RobotBase.isReal() else 4
+    RIGHT_Y_AXIS = 5
+    # Controller button mappings
+    CROSS_BUTTON = 1
+    CIRCLE_BUTTON = 2
+    L1_BUTTON = 4
+    POV_UP = 0
+    POV_DOWN = 180
 
     def __init__(self) -> None:
         self._max_speed = (
@@ -55,19 +68,6 @@ class KrakenRobotContainer:
 
         # Use CommandGenericHID for controller compatibility
         self._joystick = CommandGenericHID(0)
-
-        # Controller axis mappings
-        self.LEFT_X_AXIS = 0
-        self.LEFT_Y_AXIS = 1
-        self.RIGHT_X_AXIS = 2
-        self.RIGHT_Y_AXIS = 5
-
-        # Controller button mappings
-        self.CROSS_BUTTON = 1
-        self.CIRCLE_BUTTON = 2
-        self.L1_BUTTON = 4
-        self.POV_UP = 0
-        self.POV_DOWN = 180
 
         self.drivetrain = TunerConstants.create_drivetrain()
 
@@ -111,8 +111,7 @@ class KrakenRobotContainer:
         Trigger(DriverStation.isDisabled).whileTrue(
             self.drivetrain.apply_request(lambda: idle).ignoringDisable(True)
         )
-
-        self._joystick.b().whileTrue(
+        self._joystick.button(self.CIRCLE_BUTTON).whileTrue(
             FaceTarget(
                 self.drivetrain,
                 # Blue hub
@@ -121,6 +120,8 @@ class KrakenRobotContainer:
                 self._joystick,
                 self._max_speed,
                 self._max_angular_rate,
+                self.LEFT_Y_AXIS,
+                self.LEFT_X_AXIS,
             )
         )
 
