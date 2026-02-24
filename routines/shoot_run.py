fda123df4ec11423ae2d9c63f56ef84cf3892055
noiritgc <113562372+noiritgc@users.noreply.github.com>
2026-02-24 15:39:07 -0500

    shoot_sequence

--- routines/shoot_run.py
+++ routines/shoot_run.py
@@ -6,7 +6,6 @@ from commands.shoot_kicker import Shoot_Kicker
 from commands.run_seesaw import RunSeesaw
 from commands.goto_commands import goto_shoot_pos
 from subsystems.shooter import Shooter
-from subsystems.intake import Intake
 from subsystems.seesaw import Seesaw
 from subsystems.swerve_drivetrain import CommandSwerveDrivetrain
 
@@ -25,15 +24,22 @@ class ExtendAndIntake(ParallelCommandGroup):
         max_angular_acceleration_rads: float,
     ):
         super().__init__()
-        self.GotoShoot = goto_shoot_pos(target_point, swerve_subsystem, max_speed, max_angular_speed_rads)
+        self.GotoShoot = goto_shoot_pos(
+            target_point,
+            swerve_subsystem,
+            max_speed,
+            max_angular_speed_rads,
+            max_acceleration,
+            max_angular_acceleration_rads,
+        )
         self.addCommands(
-            ParallelCommandGroup(
-                Shoot(shooter),
-                SequentialCommandGroup(
-                    ParallelCommandGroup(
-                        RunSeesaw(seesaw, dump),
-
-                    )
+            SequentialCommandGroup(
+                ParallelCommandGroup(
+                    Shoot(shooter),
+                    SequentialCommandGroup(
+                        ParallelCommandGroup(RunSeesaw(seesaw, dump), self.GotoShoot)
+                    ),
                 ),
+                Shoot_Kicker(shooter),
             )
         )
