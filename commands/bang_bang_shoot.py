c8b402865f0843072929ca8747a4fe3d5e587a50
Drekj3 <122453860+Drekj3@users.noreply.github.com>
2026-03-19 14:32:01 -0400

    more readable bang bang

--- commands/bang_bang_shoot.py
+++ commands/bang_bang_shoot.py
@@ -10,15 +10,13 @@ class BangBangShootCommand(commands2.Command):
         super().__init__()
         self.shooter = shooter
         self.addRequirements(shooter)
-        self.desired_velocity = velocity
+        if velocity is None:
+            self.desired_velocity = self.shooter.shoot_velocity
+        else:
+            self.desired_velocity = velocity
 
     def execute(self) -> None:
-        if self.desired_velocity is None:
-            velocity = self.shooter.shoot_velocity
-        else:
-            velocity = self.desired_velocity
-
-        if velocity > self.shooter.get_shoot_velocity():
+        if self.desired_velocity > self.shooter.get_shoot_velocity():
             self.shooter.set_shoot_voltage(12)
         else:
             self.shooter.set_shoot_voltage(0)
