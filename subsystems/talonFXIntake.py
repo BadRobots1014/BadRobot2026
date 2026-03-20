997fd0401b9c0d2006746569c1fb369e2073751a
ReidS28 <strawj3434@gmail.com>
2026-03-20 16:53:17 -0400

    Move hopper to separate subsystem

--- subsystems/talonFXIntake.py
+++ subsystems/talonFXIntake.py
@@ -3,14 +3,8 @@ import threading
 from commands2 import Subsystem
 import ntcore
 from ntcore import NetworkTableInstance
-import phoenix6
-from phoenix6.controls import Follower
-from phoenix6.controls.voltage_out import VoltageOut
-from phoenix6.hardware import TalonFX
-from phoenix6.signals import MotorAlignmentValue
 
 from hardware.base.motorcontroller import MotorController
-from hardware.base.switch import LimitSwitch
 from hardware.impl.motor_controller_config import (
     MotorControllerConfig,
     MotorControllerIdleMode,
@@ -20,19 +14,11 @@ from hardware.impl.motor_controller_config import (
 INTAKE_VOLTAGE = 5.5
 DUMP_VOLTAGE = -5.0
 
-EXTENSION_VOLTAGE = 2
-
-MAX_ENCODER_ROTATIONS = 40
-
 
 class TalonIntakeSubsystem(Subsystem):
     def __init__(
         self,
         intake: MotorController,
-        right: TalonFX,
-        left: TalonFX,
-        forward: LimitSwitch,
-        backward: LimitSwitch,
     ) -> None:
         super().__init__()
         self.intake_motor = intake
@@ -42,38 +28,8 @@ class TalonIntakeSubsystem(Subsystem):
         )
         self.intake_motor.apply_configs(intake_config)
 
-        self.left = left
-        self.right = right
-
-        inverted_value = phoenix6.signals.InvertedValue.COUNTER_CLOCKWISE_POSITIVE
-
-        idle_mode = phoenix6.signals.NeutralModeValue.BRAKE
-
-        config = phoenix6.configs.TalonFXConfiguration().with_motor_output(
-            phoenix6.configs.MotorOutputConfigs()
-            .with_inverted(inverted_value)
-            .with_neutral_mode(idle_mode)
-        )
-
-        right_config = phoenix6.configs.TalonFXConfiguration().with_motor_output(
-            phoenix6.configs.MotorOutputConfigs().with_neutral_mode(idle_mode)
-        )
-
-        self.left.configurator.apply(config)
-        self.right.configurator.apply(right_config)
-
-        right.set_control(
-            Follower(left.device_id, motor_alignment=MotorAlignmentValue.OPPOSED)
-        )
-
-        self.left.get_motor_voltage().set_update_frequency(100)
-
-        self.forward = forward
-        self.backward = backward
-
         self.intake_voltage = INTAKE_VOLTAGE
         self.dump_voltage = DUMP_VOLTAGE
-        self.extension_voltage = EXTENSION_VOLTAGE
 
         # setup network tables
         self.nt_inst = NetworkTableInstance.getDefault()
@@ -89,15 +45,6 @@ class TalonIntakeSubsystem(Subsystem):
         self.dump_voltage_pub = self.dump_voltage_topic.publish()
         self.dump_voltage_pub.set(DUMP_VOLTAGE)
         self.dump_voltage_sub = self.dump_voltage_topic.subscribe(DUMP_VOLTAGE)
-
-        self.extension_voltage_topic = self.nt_table.getDoubleTopic(
-            "extension_motor_voltage"
-        )
-        self.extension_voltage_pub = self.extension_voltage_topic.publish()
-        self.extension_voltage_pub.set(EXTENSION_VOLTAGE)
-        self.extension_voltage_sub = self.extension_voltage_topic.subscribe(
-            EXTENSION_VOLTAGE
-        )
 
         self.lock = threading.Lock()
 
@@ -121,26 +68,8 @@ class TalonIntakeSubsystem(Subsystem):
             self.dump_voltage_sub, ntcore.EventFlags.kValueAll, _on_dump_voltage_changed
         )
 
-        def _on_extension_voltage_changed(event: ntcore.Event) -> None:
-            with self.lock:
-                self.extension_voltage = event.data.value.getDouble()
-                print(self.extension_voltage)
-
-        self.extension_changed_handle = self.nt_inst.addListener(
-            self.extension_voltage_sub,
-            ntcore.EventFlags.kValueAll,
-            _on_extension_voltage_changed,
-        )
-
     def periodic(self) -> None:
-        if self.backward_extended():
-            self.set_rotations(0)
-        elif self.forward_extended():
-            self.set_rotations(MAX_ENCODER_ROTATIONS)
-
-        self.nt_table.putBoolean("Forward limit: ", self.forward_extended())
-        self.nt_table.putBoolean("Backward limit: ", self.backward_extended())
-        self.nt_table.putNumber("Extension encoder", self.get_extension_position())
+        pass
 
     def set_intake_voltage_from_networktable(self) -> None:
         self.intake_motor.set_voltage(self.intake_voltage)
@@ -153,38 +82,3 @@ class TalonIntakeSubsystem(Subsystem):
 
     def set_intake_velocity(self, rpm: float) -> None:
         self.intake_motor.set_velocity(rpm)
-
-    def set_extension_voltage(self, voltage: float) -> None:
-        if (voltage > 0 and self.forward_extended()) or (
-            voltage < 0 and self.backward_extended()
-        ):
-            self.left.set_control(VoltageOut(0))
-        else:
-            self.left.set_control(VoltageOut(voltage))
-
-    def set_extension_voltage_from_networktable(self) -> None:
-        if not self.forward_extended():
-            self.left.set_control(VoltageOut(self.extension_voltage))
-        else:
-            self.left.set_control(VoltageOut(0))
-
-    def set_retraction_voltage_from_networktable(self) -> None:
-        if not self.backward_extended():
-            self.left.set_control(VoltageOut(-self.extension_voltage))
-        else:
-            self.left.set_control(VoltageOut(0))
-
-    def forward_extended(self) -> bool:
-        return self.forward.get_state()
-
-    def backward_extended(self) -> bool:
-        return self.backward.get_state()
-
-    def set_rotations(self, rotations: float = 0) -> None:
-        self.left.set_position(rotations)
-
-    def get_extension_position(self) -> float:
-        return self.left.get_position().value
-
-    def get_max_extension_value(self) -> float:
-        return MAX_ENCODER_ROTATIONS
