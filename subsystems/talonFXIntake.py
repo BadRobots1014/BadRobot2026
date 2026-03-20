b3ac0986bd0276f9e45c0156eefcd66372985ab0
Bad Coders <badcoders@dublinroboticsboosters.org>
2026-03-20 08:35:25 -0400

    change intake voltage

--- subsystems/talonFXIntake.py
+++ subsystems/talonFXIntake.py
@@ -17,7 +17,7 @@ from hardware.impl.motor_controller_config import (
 )
 
 # Dumping velocity should be 1500
-INTAKE_VOLTAGE = 4.5
+INTAKE_VOLTAGE = 5.5
 DUMP_VOLTAGE = -5.0
 
 EXTENSION_VOLTAGE = 3
