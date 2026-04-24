bb9010979c6ef6a1b3e27aa1bb498319b590b8e3
Bad Coders <badcoders@dublinroboticsboosters.org>
2026-04-24 13:23:30 -0400

    fix tests

--- kraken_container.py
+++ kraken_container.py
@@ -275,7 +275,7 @@ class KrakenRobotContainer:
         # Configure commands used in auto
         NamedCommands.registerCommand(
             "Extend",
-            ExtendHopperCommand(self._hopper, extend=True).withTimeout(1),
+            ExtendHopperCommand(self._hopper).withTimeout(1),
         )
 
         NamedCommands.registerCommand(
@@ -515,7 +515,7 @@ class KrakenRobotContainer:
 
         # manual extend
         self._auxiliary_controller.bind_pov_up("Manual extend hopper").whileTrue(
-            ExtendHopperCommand(self._hopper, extend=True)
+            ExtendHopperCommand(self._hopper)
         )
 
         # Spin up shooter L2
@@ -564,9 +564,7 @@ class KrakenRobotContainer:
         intake_wheel_in = RunIntakeCommand(self._intake, dump=False)
         self._auxiliary_controller.create_button(
             CROSS_BUTTON, "Intake wheel in"
-        ).whileTrue(
-            ExtendHopperCommand(self._hopper, extend=True).andThen(intake_wheel_in)
-        )
+        ).whileTrue(ExtendHopperCommand(self._hopper).andThen(intake_wheel_in))
 
         # Intake wheel dump (HOLD)
         intake_wheel_out = DumpRoutine(self._intake, self._kicker, self._conveyor)
@@ -591,7 +589,7 @@ class KrakenRobotContainer:
         ).whileTrue(RunShooterCommand(self._shooter, rpm=3300))
         self._test_controller.create_axis(
             L2_TRIGGER, "extend hopper test", AXIS_THRESHOLD_VALUE
-        ).whileTrue(ExtendHopperCommand(self._hopper, extend=True))
+        ).whileTrue(ExtendHopperCommand(self._hopper))
         self._test_controller.create_button(L1_BUTTON, "kicker").whileTrue(
             RunKickerCommand(self._kicker, invert=False)
         )
